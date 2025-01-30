574c9e6fc63da74961b8f8b159addcf5e6eef1cc
Askar Kasimov <59764983+AskarKasimov@users.noreply.github.com>
2025-01-30 12:33:24 +0000

    reducing commandexecutor

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            MusicBand musicBand = new MusicBand();
-            System.out.println(musicBand.getId());
-        }
-        main2();
-        MusicBand musicBand = new MusicBand();
-        System.out.println(musicBand.getId());
-    }
-
-    public static void main2() {
-        for (int i = 0; i < 10; i++) {
-            MusicBand musicBand = new MusicBand();
-            System.out.println(musicBand.getId());
-        }
-        MusicBand musicBand = new MusicBand();
-        System.out.println(musicBand.getId());
+        System.out.println("It compiles! :)");
     }
 }
