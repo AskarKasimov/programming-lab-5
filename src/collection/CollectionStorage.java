e87ee9a6efd1762df02e28f0763fb95999d14fc3
Askar Kasimov <kasimovaskar123@gmail.com>
2025-02-02 14:03:13 +0300

    CollectionStorage singleton now

--- src/collection/CollectionStorage.java
+++ src/collection/CollectionStorage.java
@@ -8,24 +8,36 @@ import java.util.Collections;
 import java.util.LinkedList;
 
 public class CollectionStorage {
-    private static final LinkedList<MusicBand> collection = new LinkedList<>();
+    private static CollectionStorage instance;
+    private final LinkedList<MusicBand> collection;
     // TODO: поменять LinkedList под вариант
 
-    public static void add(MusicBand item) {
+    private CollectionStorage() {
+        collection = new LinkedList<>();
+    }
+
+    public static CollectionStorage getInstance() {
+        if (instance == null) {
+            instance = new CollectionStorage();
+        }
+        return instance;
+    }
+
+    public void add(MusicBand item) {
         collection.add(item);
     }
 
-    public static void removeById(long id) throws NoSuchIdException {
+    public void removeById(long id) throws NoSuchIdException {
         if (!collection.removeIf(item -> item.getId() == id)) {
             throw new NoSuchIdException();
         }
     }
 
-    public static void removeByFrontMan(Person frontMan) {
+    public void removeByFrontMan(Person frontMan) {
         collection.removeIf(item -> item.getFrontMan().equals(frontMan));
     }
 
-    public static int countByNumberOfParticipants(int numberOfParticipants) {
+    public int countByNumberOfParticipants(int numberOfParticipants) {
         int count = 0;
         for (MusicBand item : collection) {
             if (item.getNumberOfParticipants() == numberOfParticipants) {
@@ -35,7 +47,7 @@ public class CollectionStorage {
         return count;
     }
 
-    public static String filterByAlbumsCount(int albumsCount) {
+    public String filterByAlbumsCount(int albumsCount) {
         StringBuilder result = new StringBuilder();
         for (MusicBand item : collection) {
             if (item.getAlbumsCount() == albumsCount) {
@@ -45,34 +57,34 @@ public class CollectionStorage {
         return result.toString();
     }
 
-    public static void updateById(long id, MusicBand newMusicBand) throws NoSuchIdException {
+    public void updateById(long id, MusicBand newMusicBand) throws NoSuchIdException {
         if (!collection.removeIf(item -> item.getId() == id)) {
             throw new NoSuchIdException();
         }
         collection.add(newMusicBand);
     }
 
-    public static void shuffle() {
+    public void shuffle() {
         Collections.shuffle(collection);
     }
 
-    public static void reorder() {
+    public void reorder() {
         Collections.reverse(collection);
     }
 
-    public static void removeAtIndex(int index) {
+    public void removeAtIndex(int index) {
         collection.remove(index);
     }
 
-    public static void clear() {
+    public void clear() {
         collection.clear();
     }
 
-    public static void save(String path) {
+    public void save(String path) {
         // TODO: реализовать сохранение коллекции в файл
     }
 
-    public static String getAllElementsToString() {
+    public String getAllElementsToString() {
         StringBuilder result = new StringBuilder();
         collection.forEach(item -> result.append(item).append("\n"));
         return result.toString();
