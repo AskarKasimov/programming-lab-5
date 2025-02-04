6769775ec27ddaf75db78d2b9c38f9a8cc0af27c
Askar Kasimov <kasimovaskar123@gmail.com>
2025-02-05 01:57:30 +0300

    new update command

--- src/main/java/ru/askar/lab5/command/UpdateCommand.java
+++ src/main/java/ru/askar/lab5/command/UpdateCommand.java
@@ -1,17 +1,40 @@
 package ru.askar.lab5.command;
 
+import ru.askar.lab5.cli.input.InputReader;
+import ru.askar.lab5.collection.CollectionStorage;
+import ru.askar.lab5.object.Ticket;
+
 public class UpdateCommand extends Command {
-    public UpdateCommand() {
-        super("update", 6);
+    private final InputReader inputReader;
+
+    public UpdateCommand(InputReader inputReader) {
+        super("update", 3);
+        this.inputReader = inputReader;
     }
 
     @Override
     public void execute(String[] args) {
-
+        Long id = Long.parseLong(args[0]);
+        Ticket oldTicket = CollectionStorage.getInstance().getCollection().get(id);
+        if (oldTicket == null) {
+            outputWriter.writeOnFail("Элемент с таким id не найден");
+            return;
+        }
+        String name = args[1];
+        long price = Long.parseLong(args[2]);
+        outputWriter.writeOnSuccess("Хотите изменить прочие данные? (y/n): ");
+        if (inputReader.getInputString().equals("y")) {
+            Ticket newTicket = Ticket.createTicket(outputWriter, inputReader, id, name, price);
+            CollectionStorage.getInstance().getCollection().put(id, newTicket);
+        } else {
+            oldTicket.setName(name);
+            oldTicket.setPrice(price);
+        }
+        outputWriter.writeOnSuccess("Элемент обновлен");
     }
 
     @Override
     public String getInfo() {
-        return "update id {element} - обновить значение элемента коллекции, id которого равен заданному";
+        return "update id name price - обновить значение элемента коллекции, id которого равен заданному";
     }
 }
